0acd4c7db645c6082f416e752b2b3cec60c28d1c
FIREFROST-11 <dsfastcar@gmail.com>
2024-08-07 11:58:13 -0400

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -6,9 +6,9 @@ namespace ContactManager.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
-#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning disable CS8618 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+#pragma warning restore CS8618 
         : base(options)
     {
     }
